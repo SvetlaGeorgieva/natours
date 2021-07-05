f5630c65b95c8f9ef84970f6bb3ffc910adc30c1
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-07-05 13:22:31 +0300

    Create separate routers: - one route for the 'tours' resourse - one route for the 'users' resourse

--- app.js
+++ app.js
@@ -155,35 +155,37 @@ const deleteUser = (req, res) => {
 };
 
 // 3) ROUTES
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
 // prettier-ignore
-app
-.route('/api/v1/tours')
-.get(getAllTours)
-.post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter
+   .route('/')
+   .get(getAllTours)
+   .post(createTour);
 
 // prettier-ignore
-app
-.route('/api/v1/users')
-.get(getAllUsers)
-.post(createUser);
+tourRouter
+   .route('/:id')
+   .get(getTour)
+   .patch(updateTour)
+   .delete(deleteTour);
 
-app
-  .route('/api/v1/users/:id')
+// prettier-ignore
+userRouter
+   .route('/')
+   .get(getAllUsers)
+   .post(createUser);
+
+// prettier-ignore
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) START THE SERVER
 const port = 3000;
