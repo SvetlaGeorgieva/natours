78212b74657d86052a14ae814347161ed6c69ad0
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-07-13 13:34:18 +0300

    Chaining middleware fuctions
    - Added an additional middleware function 'checkBody' to the post request
    for the tour resourse. It will check if the body has the name and price
    parameters before storing the new tour to the list.
    - fixed error in the path for storing a new tour to the list

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,6 +17,18 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price property',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -49,7 +61,7 @@ exports.createTour = (req, res) => {
   tours.push(newTour);
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
       // send the newly created object as a response
