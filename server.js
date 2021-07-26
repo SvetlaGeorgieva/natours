fbb4e3efed286f8881401ba4b04fc83fcec5381c
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-07-26 13:28:24 +0300

    - Add connection to mongoDB cloud database. - Use 'mongoose' library to make the connection to the DB - Set npm start to start the dev environment

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // read our custom variables from the file and save them to node's enviroment variables
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // Environment variables
 // console.log(app.get('env')); // set by express
