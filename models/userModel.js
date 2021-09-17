9dfcec7b813a4af4c352922e22204b2fbd0f66af
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-09-17 15:24:59 +0300

    1) Authotization and 2) Password reset functionality
    1) Authorization
    - add roles to the users (enum: ['user', 'guide', 'lead-guide', 'admin'])
    - restrict access for 'deleting a Tour' only to roles: 'admin', 'lead-guide'
    - 'restrictTo' handler function created in authController.js
    - include the role of the user on sign up
    
    2) Password reset functionality
    - use 'nodemailer' module for sending emails
    - use 'crypto' module for creating hashed password reset tokens
    - add email settings (host, port, etc) to config.env
    - create new email.js file. It has a sendEmail function which takes an
    options object (subject, message, mailTo properties) and actually sends
    the email.
    - create routes for '/forgotPassword' and '/resetPassword/:token' for
    the user routes
    - add passwordResetToken and passwordResetExpires fields to the the User
    model
    - authController.forgotPassword() -> a middleware for checking the user,
    creating a PasswordResetToken, creating a url for this token, and sending
    an email to the user.

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -16,6 +17,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please, provide a vaid email'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please, provide a password'],
@@ -37,6 +43,8 @@ const userSchema = new mongoose.Schema({
     type: Date,
     select: false,
   },
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 // encrypt the password on password save/update
@@ -73,6 +81,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   }
   // False means NOT changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
