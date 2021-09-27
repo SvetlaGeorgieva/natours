863ee3f749592aeb1e1c6ecb5e80fccd1aa22bbd
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-09-27 15:36:31 +0300

    Security: - install 'helmet' library to use helmet() middleware to set security http headers - install 'express-rate-limit' library to add rate-limiting middleware for all requests from the same API to root /api. Limit it to 100 requests per hour. - add comment for all global middlewares (to make clear what they are for)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,18 +10,33 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1)  MIDDLEWARES
-// process.env is available even though it is not defined with require in this file. Why? Because the process is one no matter in which file we are, and the loading of the enviroment variables needs to happen only once. Then they are available on the process. This has happened in server.js
+// 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+// ->  process.env is available even though it is not defined with require in this file. Why? Because the process is one no matter in which file we are, and the loading of the enviroment variables needs to happen only once. Then they are available on the process. This has happened in server.js
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
 // Serving static files
-// express.static() a folder which we can access to serve static files. Serves only files, not folders (can serve 'http://127.0.0.1:3000/img/pin.png', but can't serve 'http://127.0.0.1:3000/img/')
+// -> express.static() a folder which we can access to serve static files. Serves only files, not folders (can serve 'http://127.0.0.1:3000/img/pin.png', but can't serve 'http://127.0.0.1:3000/img/')
 app.use(express.static(`${__dirname}/public`));
 
-// create a middleware to add the request time to the response object
+// Test middleware: create a middleware to add the request time to the response object
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
