231b7e6ec6ea1bea46b188cabbb9459a9b49772a
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-10-06 16:15:37 +0300

    Factory functions: Update and Create - handlerFactory.js -> add factory functions updateOne and createOne to update or create one document of a certain Model type. (Model passed as an argument from the higher order function) - use the above factory handler functions in tour, review and user controller files - refactor reviewController.createReview -> separate the part for setting the tour and user id-s which come from the request data and put it in a separate middle function 'setTourUserIds'. Use this middleware before calling 'reviewController.createReview'.

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,11 +77,6 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
