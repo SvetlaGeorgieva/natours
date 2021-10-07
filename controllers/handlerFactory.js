60a3a388a981ed56c87c9d72fa6b11515c8c782b
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-10-07 15:03:21 +0300

    Factory functions for Reading; a /me endpoint - implement factory function getOne. Parameters (Model, popOptions). Will return one document from the specified model, with the ID comming from the request url. The popOptions is used to add populate options. See tourController.getTour. - implement factory function getAll. Parameter Model. Will get all documents of the specified Model type. - Make an adjustment/hack on handlerFactory.getAll method to allow for nested GET reviews on tour. - use the factory methods on tours, reviews and users. - reviewRoutes.js -> handle requests from GET review/ID endpoints - userRoutes.js, userController.js -> add a new endpoint /me. It will return all data about the currently logged in user.

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
@@ -42,6 +43,53 @@ exports.createOne = Model =>
       status: 'success',
       data: {
         data: newDoc,
+      },
+    });
+  });
+
+// popOptions is a populate options object
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) {
+      query = query.populate(popOptions);
+    }
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (hack)
+    let filter = {};
+    if (req.params.tourId) {
+      filter = { tour: req.params.tourId };
+    }
+
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const docs = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        data: docs,
       },
     });
   });
--- controllers/userController.js
+++ controllers/userController.js
@@ -13,18 +13,10 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
@@ -66,16 +58,12 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!',
+    message: 'This route is not defined! Please use /signup instead.',
   });
 };
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 
 // Do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
