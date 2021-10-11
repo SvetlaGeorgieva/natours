5e2cae9ead37c50d167a03cff757d49d38fcdd99
SvetlaGeorgieva <sv.georgieva@gmail.com>
2021-10-11 15:32:11 +0300

    Calculate average rating on tour: - create a static method 'calcAverageRatings' on the tour schema. It calculates number of reviews and average rating for a tour with a given ID. Use this static method to update fields 'ratingsQuantity' and 'ratingsAverage' on the tour when a new review is created.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -50,11 +51,34 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+// in a static method like this, the 'this' keyword points to the Model
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRatings: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRatings,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', function () {
+  // 'this' points to current review
+  // this.constructor points to the model of the document
+  this.constructor.calcAverageRatings(this.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
-
-// How we want to use the reviews:
-// POST /tour/234dadf4/reviews -> current user posts new review for this tour
-// GET /tour/234dadf4/reviews -> get all reviews for this tour
-// GET /tour/234dadf4/reviews/213ytja7 -> get this review for tour with that id
