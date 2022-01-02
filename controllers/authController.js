236c63c4b982cf90b659cee2e52b61a19f89219c
SvetlaGeorgieva <sv.georgieva@gmail.com>
2022-01-02 14:43:35 +0200

    Add default user photo.

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,6 +47,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role,
+    photo: req.body.photo,
   });
 
   createSendToken(newUser, 201, res);
